dd2ec0d296871abf576a0e9fb643d32446f2d5c5
Dmitro <dmitrii_zhurzhalin@mail.ru>
2022-03-17 09:13:04 +0300

    refactor 17_03_22

--- src/CarListTest.java
+++ src/CarListTest.java
@@ -3,7 +3,7 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class CarListTest {
-    private CarList carList;
+    public CarList carList;
 
     @org.junit.Before
     public void setUp() throws Exception {
@@ -24,5 +24,39 @@ public class CarListTest {
 
         assertTrue(carList.removeAt(1));
         assertEquals(99, carList.size());
+    }
+
+    @Test
+    public void whenElementRemovedThenSizeMustBeDecreased() {
+        Car car = new Car("test", 2);
+        carList.add(car);
+        assertEquals(101, carList.size());
+        assertTrue(carList.remove(car));
+        assertEquals(100, carList.size());
+    }
+
+    @Test
+    public void whenNonExistElementRemovedThenReturnFalse() {
+        Car car = new Car("fake", 123);
+        assertFalse(carList.remove(car));
+        assertEquals(100, carList.size());
+    }
+
+    @Test
+    public void whenListClearedThenSizeMustBe0() {
+        carList.clear();
+        assertEquals(0, carList.size());
+    }
+
+    @Test(expected = IndexOutOfBoundsException.class)
+    public void whenIndexOutOfBoundsThenThrovExeptions() {
+        carList.get(100);
+    }
+
+    @Test
+    public void getReturnRightValue() {
+        Car car = carList.get(0);
+        assertEquals("Brand0", car.getBrand());
+
     }
 }
